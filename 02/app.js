a14885680ab4e18c7592276151169d79cc2e8e21
Nicki Vance <-l>
2019-12-12 10:04:28 -0800

    Evaluate opcodes.

---
+++ 02/app.js
@@ -0,0 +1,30 @@
+let example = [
+    1, 9, 10, 70,
+    2, 3, 11, 0,
+    99,
+    30, 40, 50
+];
+
+const evalOpCodes = function (opcodes, idx = 0) {
+    switch (opcodes[idx]) {
+        case 1:
+            let a = opcodes[idx + 1];
+            let b = opcodes[idx + 2];
+            opcodes[opcodes[idx + 3]] = a + b;
+            console.log(opcodes);
+            evalOpCodes(opcodes, idx + 4);
+            break;
+        case 2:
+            let c = opcodes[idx + 1];
+            let d = opcodes[idx + 2];
+            opcodes[opcodes[idx + 3]] = c * d;
+            console.log(opcodes);
+            evalOpCodes(opcodes, idx + 4);
+            break;
+        case 99:
+            console.log("Reached opcode 99: halting program.");
+            break;
+        default:
+            console.log("There's something wrong - unidentified opcode: " + opcodes[idx]);
+    }
+}
